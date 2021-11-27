c04894b98dfaeb66a5bd78928a5a1ea4f2eb53f3
jleidel <john.leidel@gmail.com>
2021-11-27 08:44:03 -0600

    updating bootstrap script to handle 11.1.0 packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 #
 # Copyright (C) 2017-2021 Tactical Computing Laboratories, LLC
 # All Rights Reserved
@@ -8,13 +8,19 @@
 #
 
 SSTCORE_11_0_0="sstcore-11.0.0.tar.gz"
+SSTCORE_11_1_0="sstcore-11.1.0.tar.gz"
 SSTELEMENTS_11_0_0="sstelements-11.0.0.tar.gz"
+SSTELEMENTS_11_1_0="sstelements-11.1.0.tar.gz"
 
 SSTCORE_11_0_0_URL="https://github.com/sstsimulator/sst-core/releases/download/v11.0.0_Final/sstcore-11.0.0.tar.gz"
+SSTCORE_11_1_0_URL="https://github.com/sstsimulator/sst-core/releases/download/v11.1.0_Final/sstcore-11.1.0.tar.gz"
 SSTELEMENTS_11_0_0_URL="https://github.com/sstsimulator/sst-elements/releases/download/v11.0.0_Final/sstelements-11.0.0.tar.gz"
+SSTELEMENTS_11_1_0_URL="https://github.com/sstsimulator/sst-elements/releases/download/v11.1.0_Final/sstelements-11.1.0.tar.gz"
 
 SSTCORE_11_0_0_SHA1="a1d0dd4f6b0c4216c35589179da0f677bbe5fdf3"
+SSTCORE_11_1_0_SHA1="9e2d6efcf94e395555de5ceb6a5c7c19f5684892"
 SSTELEMENTS_11_0_0_SHA1="8b9e779a8ace79a2d5767692a65505c63d3c5cd1"
+SSTELEMENTS_11_1_0_SHA1="8f2050e907466f32d8e2f81f1622aab1db3011e0"
 
 GETCMD=""
 SHA1SUM=""
@@ -81,6 +87,26 @@ download_packages_11() {
   fi
 }
 
+download_packages_111() {
+  #-- SST 11.1.0
+  $GETCMD $SSTCORE_11_1_0 $SSTCORE_11_1_0_URL >> /dev/null 2>&1
+  $GETCMD $SSTELEMENTS_11_1_0 $SSTELEMENTS_11_1_0_URL >> /dev/null 2>&1
+
+  if test -f "$SSTCORE_11_1_0"; then
+    echo "Downloaded $SSTCORE_11_1_0"
+  else
+    echo "Failed to download $SSTCORE_11_1_0"
+    exit -1
+  fi
+
+  if test -f "$SSTELEMENTS_11_1_0"; then
+    echo "Downloaded $SSTELEMENTS_11_1_0"
+  else
+    echo "Failed to download $SSTELEMENTS_11_1_0"
+    exit -1
+  fi
+}
+
 verify_packages_11() {
   #-- get the sha1 of the package
   SSTCORE_SHA1=`$SHA1SUM $SSTCORE_11_0_0 | awk '{print $1}'`
@@ -102,9 +128,35 @@ verify_packages_11() {
 
 }
 
+verify_packages_111() {
+  #-- get the sha1 of the package
+  SSTCORE_SHA1=`$SHA1SUM $SSTCORE_11_1_0 | awk '{print $1}'`
+  SSTELEMENTS_SHA1=`$SHA1SUM $SSTELEMENTS_11_1_0 | awk '{print $1}'`
+
+  if [ "$SSTCORE_SHA1" = "$SSTCORE_11_1_0_SHA1" ]; then
+    echo "SSTCORE 11.1.0 SHA1 VERIFIED!"
+  else
+    echo "FAILED TO VERIFY SSTCORE 11.1.0 SHA1 HASH"
+    exit -1
+  fi
+
+  if [ "$SSTELEMENTS_SHA1" = "$SSTELEMENTS_11_1_0_SHA1" ]; then
+    echo "SSTELEMENTS 11.1.0 SHA1 VERIFIED!"
+  else
+    echo "FAILED TO VERIFY SSTCORE 11.1.0 SHA1 HASH"
+    exit -1
+  fi
+
+}
+
 setup_symlinks_11() {
   $MY_LN -fs $MY_PWD/$SSTCORE_11_0_0 ./containers/singularity/$SSTCORE_11_0_0
   $MY_LN -fs $MY_PWD/$SSTELEMENTS_11_0_0 ./containers/singularity/$SSTELEMENTS_11_0_0
+}
+
+setup_symlinks_111() {
+  $MY_LN -fs $MY_PWD/$SSTCORE_11_1_0 ./containers/singularity/$SSTCORE_11_1_0
+  $MY_LN -fs $MY_PWD/$SSTELEMENTS_11_1_0 ./containers/singularity/$SSTELEMENTS_11_1_0
 }
 
 #-- STAGE1: Setup the download mechanism
@@ -116,12 +168,15 @@ check_container_tools
 
 #-- STAGE3: Download our packages
 download_packages_11
+download_packages_111
 
 #-- STAGE4: Verify the packages
 verify_packages_11
+verify_packages_111
 
 #-- STAGE5: Setup the symlinks
 setup_symlinks_11
+setup_symlinks_111
 
 echo "Bootstrapping complete!"
 
