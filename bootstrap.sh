58040c2149d45aad7e861846421cf241987c8a55
John Leidel <john.leidel@gmail.com>
2022-07-04 19:48:10 -0500

    moving 12.1.0 to 12.0.1

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,24 +9,24 @@
 
 SSTCORE_11_0_0="sstcore-11.0.0.tar.gz"
 SSTCORE_11_1_0="sstcore-11.1.0.tar.gz"
-SSTCORE_12_1_0="sstcore-12.1.0.tar.gz"
+SSTCORE_12_0_1="sstcore-12.0.1.tar.gz"
 SSTELEMENTS_11_0_0="sstelements-11.0.0.tar.gz"
 SSTELEMENTS_11_1_0="sstelements-11.1.0.tar.gz"
-SSTELEMENTS_12_1_0="sstelements-12.1.0.tar.gz"
+SSTELEMENTS_12_0_1="sstelements-12.0.1.tar.gz"
 
 SSTCORE_11_0_0_URL="https://github.com/sstsimulator/sst-core/releases/download/v11.0.0_Final/sstcore-11.0.0.tar.gz"
 SSTCORE_11_1_0_URL="https://github.com/sstsimulator/sst-core/releases/download/v11.1.0_Final/sstcore-11.1.0.tar.gz"
-SSTCORE_12_1_0_URL="https://github.com/sstsimulator/sst-core/releases/download/v12.0.1_Final/sstcore-12.0.1.tar.gz"
+SSTCORE_12_0_1_URL="https://github.com/sstsimulator/sst-core/releases/download/v12.0.1_Final/sstcore-12.0.1.tar.gz"
 SSTELEMENTS_11_0_0_URL="https://github.com/sstsimulator/sst-elements/releases/download/v11.0.0_Final/sstelements-11.0.0.tar.gz"
 SSTELEMENTS_11_1_0_URL="https://github.com/sstsimulator/sst-elements/releases/download/v11.1.0_Final/sstelements-11.1.0.tar.gz"
-SSTELEMENTS_12_1_0_URL="https://github.com/sstsimulator/sst-elements/releases/download/v12.0.1_Final/sstelements-12.0.1.tar.gz"
+SSTELEMENTS_12_0_1_URL="https://github.com/sstsimulator/sst-elements/releases/download/v12.0.1_Final/sstelements-12.0.1.tar.gz"
 
 SSTCORE_11_0_0_SHA1="a1d0dd4f6b0c4216c35589179da0f677bbe5fdf3"
 SSTCORE_11_1_0_SHA1="9e2d6efcf94e395555de5ceb6a5c7c19f5684892"
-SSTCORE_12_1_0_SHA1="2d601101d88dfca8ad032d71e43a6ce3197ebe81"
+SSTCORE_12_0_1_SHA1="2d601101d88dfca8ad032d71e43a6ce3197ebe81"
 SSTELEMENTS_11_0_0_SHA1="8b9e779a8ace79a2d5767692a65505c63d3c5cd1"
 SSTELEMENTS_11_1_0_SHA1="8f2050e907466f32d8e2f81f1622aab1db3011e0"
-SSTELEMENTS_12_1_0_SHA1="a3917fdae7bf1c89efa5b7252f2db1527117529c"
+SSTELEMENTS_12_0_1_SHA1="a3917fdae7bf1c89efa5b7252f2db1527117529c"
 
 GETCMD=""
 SHA1SUM=""
@@ -113,22 +113,22 @@ download_packages_111() {
   fi
 }
 
-download_packages_121() {
-  #-- SST 12.1.0
-  $GETCMD $SSTCORE_12_1_0 $SSTCORE_12_1_0_URL >> /dev/null 2>&1
-  $GETCMD $SSTELEMENTS_12_1_0 $SSTELEMENTS_12_1_0_URL >> /dev/null 2>&1
+download_packages_1201() {
+  #-- SST 12.0.1
+  $GETCMD $SSTCORE_12_0_1 $SSTCORE_12_0_1_URL >> /dev/null 2>&1
+  $GETCMD $SSTELEMENTS_12_0_1 $SSTELEMENTS_12_0_1_URL >> /dev/null 2>&1
 
-  if test -f "$SSTCORE_12_1_0"; then
-    echo "Downloaded $SSTCORE_12_1_0"
+  if test -f "$SSTCORE_12_0_1"; then
+    echo "Downloaded $SSTCORE_12_0_1"
   else
-    echo "Failed to download $SSTCORE_12_1_0"
+    echo "Failed to download $SSTCORE_12_0_1"
     exit -1
   fi
 
-  if test -f "$SSTELEMENTS_12_1_0"; then
-    echo "Downloaded $SSTELEMENTS_12_1_0"
+  if test -f "$SSTELEMENTS_12_0_1"; then
+    echo "Downloaded $SSTELEMENTS_12_0_1"
   else
-    echo "Failed to download $SSTELEMENTS_12_1_0"
+    echo "Failed to download $SSTELEMENTS_12_0_1"
     exit -1
   fi
 }
@@ -175,22 +175,22 @@ verify_packages_111() {
 
 }
 
-verify_packages_121() {
+verify_packages_1201() {
   #-- get the sha1 of the package
-  SSTCORE_SHA1=`$SHA1SUM $SSTCORE_12_1_0 | awk '{print $1}'`
-  SSTELEMENTS_SHA1=`$SHA1SUM $SSTELEMENTS_12_1_0 | awk '{print $1}'`
+  SSTCORE_SHA1=`$SHA1SUM $SSTCORE_12_0_1 | awk '{print $1}'`
+  SSTELEMENTS_SHA1=`$SHA1SUM $SSTELEMENTS_12_0_1 | awk '{print $1}'`
 
-  if [ "$SSTCORE_SHA1" = "$SSTCORE_12_1_0_SHA1" ]; then
-    echo "SSTCORE 12.1.0 SHA1 VERIFIED!"
+  if [ "$SSTCORE_SHA1" = "$SSTCORE_12_0_1_SHA1" ]; then
+    echo "SSTCORE 12.0.1 SHA1 VERIFIED!"
   else
-    echo "FAILED TO VERIFY SSTCORE 12.1.0 SHA1 HASH"
+    echo "FAILED TO VERIFY SSTCORE 12.0.1 SHA1 HASH"
     exit -1
   fi
 
-  if [ "$SSTELEMENTS_SHA1" = "$SSTELEMENTS_12_1_0_SHA1" ]; then
-    echo "SSTELEMENTS 12.1.0 SHA1 VERIFIED!"
+  if [ "$SSTELEMENTS_SHA1" = "$SSTELEMENTS_12_0_1_SHA1" ]; then
+    echo "SSTELEMENTS 12.0.1 SHA1 VERIFIED!"
   else
-    echo "FAILED TO VERIFY SSTCORE 12.1.0 SHA1 HASH"
+    echo "FAILED TO VERIFY SSTCORE 12.0.1 SHA1 HASH"
     exit -1
   fi
 
@@ -206,9 +206,9 @@ setup_symlinks_111() {
   $MY_LN -fs $MY_PWD/$SSTELEMENTS_11_1_0 ./containers/singularity/$SSTELEMENTS_11_1_0
 }
 
-setup_symlinks_121() {
-  $MY_LN -fs $MY_PWD/$SSTCORE_12_1_0 ./containers/singularity/$SSTCORE_12_1_0
-  $MY_LN -fs $MY_PWD/$SSTELEMENTS_12_1_0 ./containers/singularity/$SSTELEMENTS_12_1_0
+setup_symlinks_1201() {
+  $MY_LN -fs $MY_PWD/$SSTCORE_12_0_1 ./containers/singularity/$SSTCORE_12_0_1
+  $MY_LN -fs $MY_PWD/$SSTELEMENTS_12_0_1 ./containers/singularity/$SSTELEMENTS_12_0_1
 }
 
 #-- STAGE1: Setup the download mechanism
@@ -221,17 +221,17 @@ check_container_tools
 #-- STAGE3: Download our packages
 download_packages_11
 download_packages_111
-download_packages_121
+download_packages_1201
 
 #-- STAGE4: Verify the packages
 verify_packages_11
 verify_packages_111
-verify_packages_121
+verify_packages_1201
 
 #-- STAGE5: Setup the symlinks
 setup_symlinks_11
 setup_symlinks_111
-setup_symlinks_121
+setup_symlinks_1201
 
 echo "Bootstrapping complete!"
 
