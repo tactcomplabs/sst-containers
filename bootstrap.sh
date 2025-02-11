9f75622ec56b7f005e3c7d52ab771912a2fdb642
Jeff Young <jyoung9@gatech.edu>
2025-02-10 19:55:56 -0500

    Added update for SST 14.1.0 and added a bootstrap single script

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,7 @@ SSTCORE_12_1_0="sstcore-12.1.0.tar.gz"
 SSTCORE_13_0_0="sstcore-13.0.0.tar.gz"
 SSTCORE_13_1_0="sstcore-13.1.0.tar.gz"
 SSTCORE_14_0_0="sstcore-14.0.0.tar.gz"
+SSTCORE_14_1_0="sstcore-14.1.0.tar.gz"
 SSTELEMENTS_11_0_0="sstelements-11.0.0.tar.gz"
 SSTELEMENTS_11_1_0="sstelements-11.1.0.tar.gz"
 SSTELEMENTS_12_0_1="sstelements-12.0.1.tar.gz"
@@ -21,6 +22,7 @@ SSTELEMENTS_12_1_0="sstelements-12.1.0.tar.gz"
 SSTELEMENTS_13_0_0="sstelements-13.0.0.tar.gz"
 SSTELEMENTS_13_1_0="sstelements-13.1.0.tar.gz"
 SSTELEMENTS_14_0_0="sstelements-14.0.0.tar.gz"
+SSTELEMENTS_14_1_0="sstelements-14.1.0.tar.gz"
 
 SSTCORE_11_0_0_URL="https://github.com/sstsimulator/sst-core/releases/download/v11.0.0_Final/sstcore-11.0.0.tar.gz"
 SSTCORE_11_1_0_URL="https://github.com/sstsimulator/sst-core/releases/download/v11.1.0_Final/sstcore-11.1.0.tar.gz"
@@ -29,6 +31,7 @@ SSTCORE_12_1_0_URL="https://github.com/sstsimulator/sst-core/releases/download/v
 SSTCORE_13_0_0_URL="https://github.com/sstsimulator/sst-core/releases/download/v13.0.0_Final/sstcore-13.0.0.tar.gz"
 SSTCORE_13_1_0_URL="https://github.com/sstsimulator/sst-core/releases/download/v13.1.0_Final/sstcore-13.1.0.tar.gz"
 SSTCORE_14_0_0_URL="https://github.com/sstsimulator/sst-core/releases/download/v14.0.0_Final/sstcore-14.0.0.tar.gz"
+SSTCORE_14_1_0_URL="https://github.com/sstsimulator/sst-core/releases/download/v14.1.0_Final/sstcore-14.1.0.tar.gz"
 SSTELEMENTS_11_0_0_URL="https://github.com/sstsimulator/sst-elements/releases/download/v11.0.0_Final/sstelements-11.0.0.tar.gz"
 SSTELEMENTS_11_1_0_URL="https://github.com/sstsimulator/sst-elements/releases/download/v11.1.0_Final/sstelements-11.1.0.tar.gz"
 SSTELEMENTS_12_0_1_URL="https://github.com/sstsimulator/sst-elements/releases/download/v12.0.1_Final/sstelements-12.0.1.tar.gz"
@@ -36,6 +39,7 @@ SSTELEMENTS_12_1_0_URL="https://github.com/sstsimulator/sst-elements/releases/do
 SSTELEMENTS_13_0_0_URL="https://github.com/sstsimulator/sst-elements/releases/download/v13.0.0_Final/sstelements-13.0.0.tar.gz"
 SSTELEMENTS_13_1_0_URL="https://github.com/sstsimulator/sst-elements/releases/download/v13.1.0_Final/sstelements-13.1.0.tar.gz"
 SSTELEMENTS_14_0_0_URL="https://github.com/sstsimulator/sst-elements/releases/download/v14.0.0_Final/sstelements-14.0.0.tar.gz"
+SSTELEMENTS_14_1_0_URL="https://github.com/sstsimulator/sst-elements/releases/download/v14.1.0_Final/sstelements-14.1.0.tar.gz"
 
 SSTCORE_11_0_0_SHA1="a1d0dd4f6b0c4216c35589179da0f677bbe5fdf3"
 SSTCORE_11_1_0_SHA1="9e2d6efcf94e395555de5ceb6a5c7c19f5684892"
@@ -44,6 +48,7 @@ SSTCORE_12_1_0_SHA1="e5fd18b59799ae607fc076a941aaedd9731bf33f"
 SSTCORE_13_0_0_SHA1="eaa2b06981631232cd2c22f405a61aa3fb0f1a5c"
 SSTCORE_13_1_0_SHA1="89430e296f324b040be87200b28acd323f260121"
 SSTCORE_14_0_0_SHA1="3c3f51134cc92ac7d659543aefcd8d4fd89fea28"
+SSTCORE_14_1_0_SHA1="063edf04008622df11690448161bf3cb874eb835"
 SSTELEMENTS_11_0_0_SHA1="8b9e779a8ace79a2d5767692a65505c63d3c5cd1"
 SSTELEMENTS_11_1_0_SHA1="8f2050e907466f32d8e2f81f1622aab1db3011e0"
 SSTELEMENTS_12_0_1_SHA1="a3917fdae7bf1c89efa5b7252f2db1527117529c"
@@ -51,6 +56,7 @@ SSTELEMENTS_12_1_0_SHA1="456c27e1c5f89d5d0a03c3b766706385f16d88f9"
 SSTELEMENTS_13_0_0_SHA1="1245fca5dbecd51a79e7ac330321324d86e4405e"
 SSTELEMENTS_13_1_0_SHA1="134dcc32ff117743d2a1cc494f0a2db0b729a082"
 SSTELEMENTS_14_0_0_SHA1="9206c915f873221398409c7216edeff58ccbd9cd"
+SSTELEMENTS_14_1_0_SHA1="c4db22ae04a2a1ff0b4dcef7814190fad641f399"
 
 GETCMD=""
 SHA1SUM=""
@@ -237,6 +243,25 @@ download_packages_1400() {
   fi
 }
 
+download_packages_1410() {
+  #-- SST 14.1.0
+  $GETCMD $SSTCORE_14_1_0 $SSTCORE_14_1_0_URL >> /dev/null 2>&1
+  $GETCMD $SSTELEMENTS_14_1_0 $SSTELEMENTS_14_1_0_URL >> /dev/null 2>&1
+
+  if test -f "$SSTCORE_14_1_0"; then
+    echo "Downloaded $SSTCORE_14_1_0"
+  else
+    echo "Failed to download $SSTCORE_14_1_0"
+    exit -1
+  fi
+
+  if test -f "$SSTELEMENTS_14_1_0"; then
+    echo "Downloaded $SSTELEMENTS_14_1_0"
+  else
+    echo "Failed to download $SSTELEMENTS_14_1_0"
+    exit -1
+  fi
+}
 verify_packages_11() {
   #-- get the sha1 of the package
   SSTCORE_SHA1=`$SHA1SUM $SSTCORE_11_0_0 | awk '{print $1}'`
@@ -384,6 +409,26 @@ verify_packages_1400() {
 
 }
 
+verify_packages_1410() {
+  #-- get the sha1 of the package
+  SSTCORE_SHA1=`$SHA1SUM $SSTCORE_14_1_0 | awk '{print $1}'`
+  SSTELEMENTS_SHA1=`$SHA1SUM $SSTELEMENTS_14_1_0 | awk '{print $1}'`
+
+  if [ "$SSTCORE_SHA1" = "$SSTCORE_14_1_0_SHA1" ]; then
+    echo "SSTCORE 14.1.0 SHA1 VERIFIED!"
+  else
+    echo "FAILED TO VERIFY SSTCORE 14.1.0 SHA1 HASH"
+    exit -1
+  fi
+
+  if [ "$SSTELEMENTS_SHA1" = "$SSTELEMENTS_14_1_0_SHA1" ]; then
+    echo "SSTELEMENTS 14.1.0 SHA1 VERIFIED!"
+  else
+    echo "FAILED TO VERIFY SSTELEMENTS 14.1.0 SHA1 HASH"
+    exit -1
+  fi
+
+
 setup_symlinks_11() {
   $MY_LN -fs $MY_PWD/$SSTCORE_11_0_0 ./containers/singularity/$SSTCORE_11_0_0
   $MY_LN -fs $MY_PWD/$SSTELEMENTS_11_0_0 ./containers/singularity/$SSTELEMENTS_11_0_0
@@ -419,6 +464,11 @@ setup_symlinks_1400() {
   $MY_LN -fs $MY_PWD/$SSTELEMENTS_14_0_0 ./containers/singularity/$SSTELEMENTS_14_0_0
 }
 
+setup_symlinks_1410() {
+  $MY_LN -fs $MY_PWD/$SSTCORE_14_1_0 ./containers/singularity/$SSTCORE_14_1_0
+  $MY_LN -fs $MY_PWD/$SSTELEMENTS_14_1_0 ./containers/singularity/$SSTELEMENTS_14_1_0
+}
+
 #-- STAGE1: Setup the download mechanism
 setup_get
 setup_sha1
@@ -434,6 +484,7 @@ download_packages_1210
 download_packages_1300
 download_packages_1310
 download_packages_1400
+download_packages_1410
 
 #-- STAGE4: Verify the packages
 verify_packages_11
@@ -443,6 +494,7 @@ verify_packages_1210
 verify_packages_1300
 verify_packages_1310
 verify_packages_1400
+verify_packages_1410
 
 #-- STAGE5: Setup the symlinks
 setup_symlinks_11
@@ -452,6 +504,7 @@ setup_symlinks_1210
 setup_symlinks_1300
 setup_symlinks_1310
 setup_symlinks_1400
+setup_symlinks_1410
 
 echo "Bootstrapping complete!"
 
